d56308faa1063298f9fcc385a978d7f6be542f5e
Michael Kuehl <hello@txs.vc>
2021-01-07 13:51:58 +0100

    upgraded pom dependencies

--- src/main/java/com/example/springbootmanagementexample/LoadRestController.java
+++ src/main/java/com/example/springbootmanagementexample/LoadRestController.java
@@ -2,44 +2,45 @@ package com.example.springbootmanagementexample;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.Random ;
+import java.util.Random;
 
 @RestController
 public class LoadRestController {
 
 	private Random r = new Random();
-	
-	static String fib(int n) 
-	{ 
-			// Declare an array to store Fibonacci numbers.
-			int f[] = new int[n+2]; // 1 extra to handle case, n = 0 
-			int i; 
-					
-			// 0th and 1st number of the series are 0 and 1
-			f[0] = 0; 
-			f[1] = 1; 
-				
-			for (i = 2; i <= n; i++) 
-			{ 
-					// Add the previous 2 numbers in the series and store it
-					f[i] = f[i-1] + f[i-2]; 
-			} 
-					
-			return f.toString() ;
-	} 
-      
-  @GetMapping("/load")
+
+	static String fib(int n) {
+		// Declare an array to store Fibonacci numbers.
+		int f[] = new int[n + 2]; // 1 extra to handle case, n = 0
+		int i;
+		
+		// 0th and 1st number of the series are 0 and 1
+		f[0] = 0;
+		f[1] = 1;
+
+		for (i = 2; i <= n; i++) {
+			// Add the previous 2 numbers in the series and store it
+			f[i] = f[i - 1] + f[i - 2];
+		}
+
+		return "fib("+n+")-> "+f[n];
+	}
+
+	@GetMapping("/load")
 	public String load() {
-	    
-	    int sleep = r.nextInt(2000) ;
-	    
-	    try { 
-					System.out.println("Sleeping for " + sleep + "ms.") ;
-					// sleep, then do some work ...
-	        Thread.currentThread().sleep(sleep) ;      
-	    } catch(Exception e) {
-	    }
-	    
-	    return fib(sleep) ;
+
+		int n = r.nextInt(2000);
+
+		try {
+			// sleep, then do some work ...
+			System.out.println("Sleeping for " + n + "ms.");
+
+			Thread.currentThread();
+			Thread.sleep(n);
+		} catch (Exception e) {
+			// don't care
+		}
+
+		return fib(n);
 	}
 }
